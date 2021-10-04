d380108f62b457ffe03e52598de79ab66fe44bc4
ArtemZHR <artem.zhiruk@hotmail.com>
2021-10-04 11:47:27 +0300

    first commit

---
+++ src/components/Dialog.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-dialog
+    v-model="dialogState"
+    max-width="580"
+    persistent
+  >
+    <div class="rounded-lg white overflow-hidden">
+      <div class="dialog-close d-flex align-center justify-space-between px-5 py-3">
+        <span class="text-h4 purple--text">{{titleText}}</span>
+        <v-btn
+          @click="$emit('cancelDialog')"
+          icon
+          title="Close dialog window"
+        >
+          <v-icon
+            class="tile-refresh"
+            color="purple"
+          >mdi-close
+          </v-icon>
+        </v-btn>
+      </div>
+      <slot></slot>
+    </div>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'Dialog',
+  props: {
+    state: {
+      required: true
+    },
+    titleText: {
+      type: String
+    }
+  },
+  computed: {
+    dialogState: {
+      get () {
+        return this.state
+      },
+      set (val) {
+        this.$emit('cancelDialog')
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .dialog-close{
+    width: 100%;
+  }
+</style>
---
+++ src/components/TaskTile.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="">
+        $END$
+    </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+        $END$
+    };
+  },
+};
+</script>
